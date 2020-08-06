3f397fc3bd49f2c4a9083ee51c94032bab50e5c8
anmolsharma1214 <60529999+anmolsharma1214@users.noreply.github.com>
2020-08-06 11:20:39 -0400

    HTML PROJECT

---
+++ Assignment 2/process-form.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="utf-8">
+      <title>Your pizza is being prepared</title>
+      <link href="https://fonts.googleapis.com/css?family=Satisfy&display=swap" rel="stylesheet">
+      <link href="css/styles.css" rel="stylesheet">
+   </head>
+   <body>
+      <header>
+         <div>
+            <h1>Scrumptious Pizza</h1>
+            <h2>Hot and Fresh</h2>
+         </div>
+         <img src="image/icon.jpg" alt="pizza logo">
+      </header>
+      <main>
+         <div class="banner">
+            <h3>Pizza is being prepared!</h3>
+         </div>
+         <?php
+            first-name = $get['first-name'];
+            last-name = $get['last-name'];
+
+            echo('<p class="message">Thank You for ordering :) , your order has been confirmed. Total Amount:$15</p>');
+            ?>
+      </main>
+      <footer>
+         <p><small> © 2020 Pizza. All rights reserved.</small></p>
+      </footer>
+   </body>
+</html>
